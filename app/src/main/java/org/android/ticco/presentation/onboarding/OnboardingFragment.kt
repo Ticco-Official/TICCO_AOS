273234db1960fa8de63b8530ebcbaa61ef5898ab
serim53 <tpfla9953@naver.com>
2022-08-04 23:15:15 +0900

    [FEAT] 온보딩 화면 구현

---
+++ app/src/main/java/org/android/ticco/presentation/onboarding/OnboardingFragment.kt
@@ -0,0 +1,31 @@
+package org.android.ticco.presentation.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import org.android.ticco.R
+import org.android.ticco.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment : Fragment() {
+
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewPagerAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = viewPagerAdapter
+        binding.indicator.attachTo(binding.viewPager)
+    }
+}
