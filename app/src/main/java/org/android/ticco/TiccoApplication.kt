cacf748c7e5c78c68402882c0f3fa766929ff934
serim53 <tpfla9953@naver.com>
2022-08-06 23:14:25 +0900

    [FEAT] 카카오 로그인 링크 연결

--- app/src/main/java/org/android/ticco/TiccoApplication.kt
+++ app/src/main/java/org/android/ticco/TiccoApplication.kt
@@ -1,12 +1,13 @@
 package org.android.ticco
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class TiccoApplication: Application() {
-
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
     }
 }
