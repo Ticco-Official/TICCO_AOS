9b7fa04e6ebd94f0f7ee726fb60222d9a0b96012
안나영 <dpswpfskdud@naver.com>
2022-10-10 17:06:18 +0900

    [FEAT] 홈화면 카테고리 및 정렬 기능 구현 & 이미지 저장 (#15)

--- app/src/main/java/org/android/ticco/data/datasource/remote/AuthInterceptor.kt
+++ app/src/main/java/org/android/ticco/data/datasource/remote/AuthInterceptor.kt
@@ -7,8 +7,7 @@ import org.android.ticco.data.datasource.local.SharedPreferences
 class AuthInterceptor(private val preferences: SharedPreferences) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer " + "eyJhbGciOiJIUzUxMiJ9.eyJVU0VSX0lEIjozLCJleHAiOjE2OTY1MDQ5NjF9.4WRh-FseWmVVhgn6UuyfTDWo2LsOc-A65DoXkPjF6A5KdAXqwWDw2JptFqcVzlXgZ4S1jLz1hjaO95iWJljVZQ").build()
-
+            .addHeader("Authorization", "Bearer " + preferences.accessToken).build()
         return chain.proceed(request)
     }
 }
